6b80ce063dbedf093b102bf0571998a7b11562cf
vreka <68538271+RekaVoelfinger@users.noreply.github.com>
2020-09-01 12:54:09 +0200

    Listing files put into functions
    collect_files, print_files are created

--- googledrive/quickstart_edited.py
+++ googledrive/quickstart_edited.py
@@ -12,17 +12,6 @@ from googleapiclient.http import MediaIoBaseDownload
 # Originaly was: SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
-def download_file(current_service, file_id):
-    request = current_service.files().get_media(fileId=file_id)
-    # original: fh = io.BytesIO()
-    fh = io.FileIO("downloaded_file", 'wb')
-    downloader = MediaIoBaseDownload(fh, request)
-    print("Download requested file_id: %s" % file_id)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        print("Download %d%%." % int(status.progress() * 100))
-
 def load_creds():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -44,28 +33,39 @@ def load_creds():
             pickle.dump(creds, token)
     return creds
 
-def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    service = build('drive', 'v3', credentials=load_creds())
-
+def collect_files(current_service, page_size):
     # Call the Drive v3 API
     # files.list searches and prints names and ids of the first 10 files from drive the user has access to.
-    results = service.files().list(
-        corpora="user", # TODO To search in shared drive add parameter: driveId="1t7H5baSoNLZA_B5XZ-L6WYWDRw2sxdU9",
+    results = current_service.files().list(
+        corpora="user",  # TODO To search in shared drive add parameter: driveId="1t7H5baSoNLZA_B5XZ-L6WYWDRw2sxdU9",
         includeItemsFromAllDrives="true",
         supportsAllDrives="true",
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
+        pageSize=page_size, fields="nextPageToken, files(id, name)").execute()
+    return results.get('files', [])
 
-    if not items:
+def print_files(file_list):
+    if not file_list:
         print('No files found.')
     else:
         print('Files:')
-        for item in items:
+        for item in file_list:
             print(u'{0} ({1})'.format(item['name'], item['id']))
 
+def download_file(current_service, file_id):
+    request = current_service.files().get_media(fileId=file_id)
+    # original: fh = io.BytesIO()
+    fh = io.FileIO("downloaded_file", 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    print("Download requested file_id: %s" % file_id)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+
+
+def main():
+    service = build('drive', 'v3', credentials=load_creds())
+    print_files(collect_files(service, 5))
     download_file(service, '1CEt3H89l4003eNmuznLI4oCsMYtbxg_j')
 
 
